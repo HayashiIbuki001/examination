57e6263b47e5e7837990cedbb0653924d79c1d0f
HayashiIbuki001 <sch-w4meleer@outlook.jp>
2025-06-10 14:00:39 +0900

    持続ダメの敵の仕組みできた

--- Assets/Script/Player/PlayerHealth.cs
+++ Assets/Script/Player/PlayerHealth.cs
@@ -12,6 +12,10 @@ public class PlayerHealth : MonoBehaviour
     /// <summary> –³“GŠÔ‚©‚Ç‚¤‚© </summary>
     private bool isInvincible = false;
 
+    /// <summary> ‘±ƒ_ƒ[ƒW‚Ì•b”ŠÔŠu </summary>
+    [SerializeField] private float damageInterval = 1.0f;
+    private float damageTimer = 0f;
+
     [SerializeField] private Slider hpBar;
 
     void Start()
@@ -44,6 +48,23 @@ public class PlayerHealth : MonoBehaviour
         }
     }
 
+    void DamageZone(int damage)
+    {
+        if (currentHealth <= 0) return;
+
+        currentHealth -= damage;
+        currentHealth = Mathf.Max(currentHealth, 0);
+
+        hpBar.value = currentHealth;
+
+        if (currentHealth <= 0)
+        {
+            GameManager.Instance.isGameOver = true;
+        }
+    }
+
+    
+
     /// <summary> –³“GŠÔ‚Ìˆ— </summary>
     private IEnumerator InvincibilityTime()
     {
@@ -52,11 +73,34 @@ public class PlayerHealth : MonoBehaviour
         isInvincible = false;
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.TryGetComponent<IDamageDealer>(out var dealer))
+        if (collision.gameObject.CompareTag("Enemy"))
         {
-            TakeDamage(dealer.Damage);
+            if (collision.gameObject.TryGetComponent<IDamageDealer>(out var dealer))
+            {
+                TakeDamage(dealer.Damage);
+            }
         }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("DamageZone")) // ‘±ƒ_ƒƒ][ƒ“‚É“ü‚é
+        {
+            if (collision.gameObject.TryGetComponent<IDamageDealer>(out var dealer))
+            {
+                damageTimer += Time.deltaTime;
+                if (damageTimer >= damageInterval)
+                {
+                    DamageZone(dealer.Damage);
+                    damageTimer = 0f; // ‰Šú‰»
+                }
+            }
+        }
+        else
+        {
+            damageTimer = 0f;
+        } // ‘±ƒ_ƒƒ][ƒ“‚©‚ço‚½
     }
 }
