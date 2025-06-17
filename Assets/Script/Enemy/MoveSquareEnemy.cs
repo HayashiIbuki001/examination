77393605e09e3fb41cb377df61cf13b676403759
HayashiIbuki001 <sch-w4meleer@outlook.jp>
2025-06-17 18:58:04 +0900

    鳥亜種の見た目完成

--- Assets/Script/Enemy/MoveSquareEnemy.cs
+++ Assets/Script/Enemy/MoveSquareEnemy.cs
@@ -1,3 +1,4 @@
+using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class MoveSquareEnemy : MonoBehaviour
@@ -12,6 +13,8 @@ public class MoveSquareEnemy : MonoBehaviour
     private int directionIndex = 0;
     private Vector3[] directions;
 
+    public SquareEnemySprite spriteScript;
+
     void Start()
     {
         transform.position += Vector3.left * (moveDistanceX / 2f); // ‰º•Ó¶’[‚ÉˆÚ“®
@@ -20,7 +23,6 @@ public class MoveSquareEnemy : MonoBehaviour
         directions = new Vector3[]{
             Vector3.right, Vector3.up, Vector3.left, Vector3.down
         };
-
     }
 
     void Update()
@@ -32,16 +34,30 @@ public class MoveSquareEnemy : MonoBehaviour
 
         switch (directionIndex)
         {
-            case 0: if (offset.x >= moveDistanceX) { NextDirection(); } break; // right
-            case 1: if (offset.y >= moveDistanceY) { NextDirection(); } break; // up
-            case 2: if (offset.x <= 0f)            { NextDirection(); } break; // left
-            case 3: if (offset.y <= 0f)            { NextDirection(); } break; // down
+            case 0: if (offset.x >= moveDistanceX) 
+                {
+                    NextDirection();
+                } break; // right
+            case 1: if (offset.y >= moveDistanceY) 
+                { 
+                    NextDirection();
+                } break; // up
+            case 2: if (offset.x <= 0f)            
+                {
+                    NextDirection(); 
+                } break; // left
+            case 3: if (offset.y <= 0f)           
+                { 
+                    NextDirection(); 
+                } break; // down
         }
 
         void NextDirection()
         {
             directionIndex = (directionIndex + 1) % 4;
             if (directionIndex == 0) startPos = transform.position; //‰Šú‰»
+            
+            spriteScript.Direction(directionIndex);
         }
     }
 }
